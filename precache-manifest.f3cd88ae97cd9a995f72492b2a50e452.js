52afc041731d253d1a4e1837bfc7beb70f482330
bi198 <bbgk198@gmail.com>
2024-07-26 15:31:58 +0900

    Updates

---
+++ precache-manifest.f3cd88ae97cd9a995f72492b2a50e452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eefeb4c5a920dc24f8c08778b355751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70fb305f09371a1da25d",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "338e774e06d41ef987a5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "70fb305f09371a1da25d",
+    "url": "/static/js/2.33b32f74.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "338e774e06d41ef987a5",
+    "url": "/static/js/main.597678e5.chunk.js"
+  },
+  {
+    "revision": "b3d99bc629c5a9184875",
+    "url": "/static/js/runtime~main.2091b4db.js"
+  }
+]);
